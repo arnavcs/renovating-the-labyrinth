6756d9718eff0a3ae68136884c064112d6e4bb45
Arnav Kumar <arnav.kumar.sri@gmail.com>
2024-10-06 16:28:22 -0400

    added map and reset and help

--- game.js
+++ game.js
@@ -15,8 +15,12 @@ const CARDINAL_NEIGHBOURS = [[0, 1], [0, -1], [1, 0], [-1, 0]];
 /**************************/
 /* global state variables */
 /**************************/
-let changed = false;
-let queuePlace = [];
+let queuePlace;
+let score;
+let remainingTime;
+let gameloopInterval;
+let timerLoop;
+let cmap;
 
 /*******************/
 /* raycasting args */
@@ -26,11 +30,13 @@ let camera = { fov: 120 };
 let canvas;
 let options = {
   viewDist: 10, // distance till stop of light
-  wallColours: ["#de4c63", "#ff9f74"],
-  zeroColour: "#31112d",
+  wallColours: ["#ae5d40", "#c77b58"],
+  zeroColour: "#4b3d44",
   dithering: true,
   pixels: 40000
 };
+let textColour = "#d2c9a5";
+let pointColour = "#d1b187";
 
 /**************/
 /* user input */
@@ -40,6 +46,7 @@ let leftPressed = false;
 let upPressed = false;
 let downPressed = false;
 let spacePressed = false;
+let hPressed = false;
 
 function keyHandler (keySet) {
   return (event) => {
@@ -62,6 +69,12 @@ function keyHandler (keySet) {
         break;
       case "Space":
         spacePressed = keySet;
+        break;
+      case "KeyR":
+        if (keySet) startGame();
+        break;
+      case "KeyH":
+        hPressed = keySet;
         break;
     }
   };
@@ -125,9 +138,124 @@ function generateMaze (side) {
   camera.dir = vnormalize([1, 1]);
 }
 
+/*********************/
+/* score calculation */
+/*********************/
+function calculateScore () {
+  cmap = map.map(a => a.map(b => b));
+  score = 0;
+
+  // find horizontals first then verticals
+  for (dir = 0; dir < 2; dir++) {
+  for (i = 1; i < cmap.length - 1; i++) {
+    let currentLength = -Infinity;
+    for (j = 0; j < cmap.length; j++) {
+      if (mapAt(cmap, dir ? [i, j] : [j, i]) == 0) {
+        if (mapAt(cmap, dir ? [i-1, j] : [j, i-1]) > 0 && 
+            mapAt(cmap, dir ? [i+1, j] : [j, i+1]) > 0) {
+          currentLength++;
+        } else {
+          currentLength = -Infinity;
+        }
+      } else {
+        if (currentLength > 0) {
+          for (let k = 1; k <= currentLength; k++) {
+            mapSet(cmap, dir ? [i, j-k] : [j-k, i], -1);
+          }
+          score += currentLength * currentLength;
+        }
+        currentLength = 0;
+      }
+    }
+  }
+  }
+}
+
+/******************/
+/* canvas overlay */
+/******************/
+function drawOverlay () {
+  let ctx = canvas.getContext("2d");
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+  ctx.fillStyle = textColour;
+  ctx.strokeStyle = options.zeroColour;
+  ctx.lineWidth = 3;
+  ctx.textBaseline = "top";
+
+  let writeText = (text, x, y) => {
+    ctx.strokeText(text, x, y);
+    ctx.fillText(text, x, y);
+  };
+
+  ctx.textAlign = "right";
+  ctx.font = "50px monospace";
+
+  writeText("✦", canvas.width - 25, 80);
+  writeText("⏱", canvas.width - 13, 20);
+  writeText(score, canvas.width - 80, 80);
+  writeText(remainingTime, canvas.width - 80, 20);
+
+  ctx.textAlign = "left";
+  ctx.font = "30px monospace";
+
+  writeText("H: Help", 15, 15);
+
+  if (remainingTime == 0) drawMap(ctx);
+
+  if (hPressed) {
+    ctx.fillStyle = options.zeroColour;
+    ctx.fillRect(40, canvas.height / 2 - 160, canvas.width - 80, 280);
+
+    ctx.fillStyle = textColour;
+    ctx.textBaseline = "middle";
+
+    writeText("WASD + Mouse:", 60, canvas.height / 2 - 120);
+    writeText("Move", canvas.width / 4, canvas.height / 2 - 120);
+    writeText("Space:", 60, canvas.height / 2 - 80);
+    writeText("Summon wall at current square", canvas.width / 4, canvas.height / 2 - 80);
+    writeText("R:", 60, canvas.height / 2 - 40);
+    writeText("Reset", canvas.width / 4, canvas.height / 2 - 40);
+
+    writeText("Summon walls to split the labyrinth into rectangular chambers.", 60, canvas.height / 2 + 40);
+    writeText("A chamber of area A contributes A² points.", 60, canvas.height / 2 + 80);
+  }
+}
+
+function drawMap (ctx) {
+  let mapSize = Math.min(canvas.height, canvas.width) / 1.5;
+  let topleft = vvplus(svtimes(mapSize / 2, [-1, -1]), 
+                       [canvas.width / 2, canvas.height / 2]);
+  let squares = map.length + 2;
+  let squareSize = mapSize / squares;
+
+  ctx.fillStyle = options.zeroColour;
+  ctx.fillRect(topleft[0], topleft[1], mapSize, mapSize);
+  for (let row = 0; row < cmap.length; row++) {
+    for (let col = 0; col < cmap.length; col++) {
+      if (cmap[row][col] > 0) {
+        ctx.fillStyle = options.wallColours[cmap[row][col] - 1];
+        ctx.fillRect(topleft[0] + squareSize * (1 + col), topleft[1] + squareSize * (1 + row), squareSize, squareSize);
+      }
+      if (cmap[row][col] < 0) {
+        ctx.fillStyle = pointColour;
+        ctx.fillRect(topleft[0] + squareSize * (1 + col), topleft[1] + squareSize * (1 + row), squareSize, squareSize);
+      }
+    }
+  }
+}
+
 /************/
 /* gameloop */
 /************/
+function decreaseTimer () {
+  remainingTime--;
+  if (remainingTime < 0) {
+    clearInterval(timerLoop);
+    clearInterval(gameloopInterval);
+  }
+}
+
 function gameloop () {
   // place blocks
   if (spacePressed) {
@@ -153,28 +281,41 @@ function gameloop () {
   if (desiredMovement[0] != 0 || desiredMovement[1] != 0) {
     let moveTo = vvplus(camera.pos, svtimes(MOVE_SPEED / 100, desiredMovement));
     camera.pos = moveTo;
-    changed = true;
   }
 
-  // re-rendering
-  if (changed) {
-    console.log(queuePlace);
-    for (let i = queuePlace.length - 1; i >= 0; i--) {
-      let place = queuePlace[i];
-      if (comfortablyOutside(place, camera.pos)) {
-        queuePlace.splice[i, 1];
-        mapSet(map, place, 2);
-      }
+  // handling placing blocks
+  for (let i = queuePlace.length - 1; i >= 0; i--) {
+    let place = queuePlace[i];
+    if (comfortablyOutside(place, camera.pos)) {
+      queuePlace.splice(i, 1);
+      mapSet(map, place, 2);
+      calculateScore();
     }
-
-    render(map, camera, canvas, options);
-    changed = false;
   }
+
+  render(map, camera, canvas, options);
+  drawOverlay();
 }
 
 /*******************/
 /* initial startup */
 /*******************/
+function startGame () {
+  generateMaze(10);
+
+  queuePlace = [];
+  calculateScore();
+  remainingTime = 120;
+
+  clearInterval(timerLoop);
+  clearInterval(gameloopInterval);
+  timerLoop = setInterval(decreaseTimer, 1000);
+  gameloopInterval = setInterval(gameloop, 50);
+
+  render(map, camera, canvas, options);
+  drawOverlay();
+}
+
 window.onload = function () {
   document.body.style.backgroundColor = options.zeroColour;
 
@@ -190,9 +331,7 @@ window.onload = function () {
   document.addEventListener("keyup", keyHandler(false), false);
   document.addEventListener("pointerlockchange", lockChangeAlert, false);
 
-  generateMaze(10);
-  setInterval(gameloop, 50);
-  render(map, camera, canvas, options);
+  startGame();
 }
 
 
